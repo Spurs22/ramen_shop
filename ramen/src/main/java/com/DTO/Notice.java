1c54f860cca3c2023d22d53528d5630ef23b9e11
유진 <@DESKTOP-BB1DHV7>
2023-05-11 17:47:04 +0900

    공지사항 수정 - 정유진

---
+++ ramen/src/main/java/com/DTO/Notice.java
@@ -0,0 +1,71 @@
+package com.DTO;
+
+public class Notice {
+	private long id;
+	private long member_id;
+	private String subject;
+	private String content;
+	private String ip_address;
+	private int category;
+	private int hitCount;
+	private int notice;
+	private String create_date;
+	
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(long member_id) {
+		this.member_id = member_id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getIp_address() {
+		return ip_address;
+	}
+	public void setIp_address(String ip_address) {
+		this.ip_address = ip_address;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+	public int getNotice() {
+		return notice;
+	}
+	public void setNotice(int notice) {
+		this.notice = notice;
+	}
+	public String getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(String create_date) {
+		this.create_date = create_date;
+	}
+	
+	
+}
