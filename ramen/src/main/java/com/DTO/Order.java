9cb11e07ef08fecb87a0b67c2f8495baad4325b3
JiYoon <tkstpqpfldk9@naver.com>
2023-05-11 16:28:49 +0900

    김지윤 - cart/order DTO 수정

--- ramen/src/main/java/com/DTO/Order.java
+++ ramen/src/main/java/com/DTO/Order.java
@@ -1,4 +1,82 @@
 package com.DTO;
 
 public class Order {
+	private int id;
+	private String status_name;
+	private int member_id;
+	private int delivery_id;
+	private String created_date;
+	private int product_id;
+	private int order_id;
+	private int status_id;
+	private int quantity;
+	private int price;
+	private int final_price;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatus_name() {
+		return status_name;
+	}
+	public void setStatus_name(String status_name) {
+		this.status_name = status_name;
+	}
+	public int getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+	public int getDelivery_id() {
+		return delivery_id;
+	}
+	public void setDelivery_id(int delivery_id) {
+		this.delivery_id = delivery_id;
+	}
+	public String getCreated_date() {
+		return created_date;
+	}
+	public void setCreated_date(String created_date) {
+		this.created_date = created_date;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public int getStatus_id() {
+		return status_id;
+	}
+	public void setStatus_id(int status_id) {
+		this.status_id = status_id;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getFinal_price() {
+		return final_price;
+	}
+	public void setFinal_price(int final_price) {
+		this.final_price = final_price;
+	}
 }
