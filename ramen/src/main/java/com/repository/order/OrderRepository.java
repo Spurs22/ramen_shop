bb77e8c0856b8bbb0afa9cc076ada7c6a020b892
JiYoon0712 <tkstpqpfldk9@naver.com>
2023-05-13 23:26:20 +0900

    Merge branch 'main' of https://github.com/Spurs22/ramen_shop.git into main

--- ramen/src/main/java/com/repository/order/OrderRepository.java
+++ ramen/src/main/java/com/repository/order/OrderRepository.java
@@ -19,13 +19,13 @@ public interface OrderRepository {
 	// 송장번호 만들기
 	void setDeliveryNumber(Long orderId, Long deliveryId);
 	
-	// 상태별 주문내역 확인
+	// 전체 주문내역 확인
 	List<Order> findOrderByMemberId();
 	
 	// 회원 > 주문내역 확인
 	List<Order> findOrderByMemberId(Long memberId);
 
-	// 전체 주문내역 확인
+	// 상태별 주문내역 확인
 	List<Order> findOrderByMemberId(String statusName);
 	
 	// 주문번호 > 주문내역 확인
