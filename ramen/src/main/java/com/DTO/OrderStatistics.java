3ffcdd1e95e7f77a0d44596f6f5db4dc0163d510
wlgus <wlgus@DESKTOP-FJL0SS1>
2023-05-16 17:23:07 +0900

    최지현 - orderStatistics DTO 추가

---
+++ ramen/src/main/java/com/DTO/OrderStatistics.java
@@ -0,0 +1,42 @@
+package com.DTO;
+
+public class OrderStatistics {
+	private Long sumquantity;
+	private Long sumprice;
+	private Long sumfinal_price;
+	private Long productid;
+	private String productname;
+	
+	public Long getSumquantity() {
+		return sumquantity;
+	}
+	public void setSumquantity(Long sumquantity) {
+		this.sumquantity = sumquantity;
+	}
+	public Long getSumprice() {
+		return sumprice;
+	}
+	public void setSumprice(Long sumprice) {
+		this.sumprice = sumprice;
+	}
+	public Long getSumfinal_price() {
+		return sumfinal_price;
+	}
+	public void setSumfinal_price(Long sumfinal_price) {
+		this.sumfinal_price = sumfinal_price;
+	}
+	public Long getProductid() {
+		return productid;
+	}
+	public void setProductid(Long productid) {
+		this.productid = productid;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	
+	
+}
