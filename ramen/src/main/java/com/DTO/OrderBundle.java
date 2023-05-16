52337fd6b1b8fde91ba7ff64810ed7194789c624
wlgus <wlgus@DESKTOP-FJL0SS1>
2023-05-16 16:25:22 +0900

    최지현 - orderBundleDTO, orderItemDTO 추가

---
+++ ramen/src/main/java/com/DTO/OrderBundle.java
@@ -0,0 +1,110 @@
+package com.DTO;
+
+import java.util.List;
+
+public class OrderBundle {
+	private Long orderBundleId;
+	private Long memberId;
+	private Long deliveryId;
+	private String createdDate;
+	
+	private String receiveName;
+	private String tel;
+	private String postNum;
+	private String address1;
+	private String address2;
+	
+	private String userEmail;
+
+	private List<OrderItem> orderItems;
+
+	public Long getOrderBundleId() {
+		return orderBundleId;
+	}
+
+	public void setOrderBundleId(Long orderBundleId) {
+		this.orderBundleId = orderBundleId;
+	}
+
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+
+	public Long getDeliveryId() {
+		return deliveryId;
+	}
+
+	public void setDeliveryId(Long deliveryId) {
+		this.deliveryId = deliveryId;
+	}
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getReceiveName() {
+		return receiveName;
+	}
+
+	public void setReceiveName(String receiveName) {
+		this.receiveName = receiveName;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getPostNum() {
+		return postNum;
+	}
+
+	public void setPostNum(String postNum) {
+		this.postNum = postNum;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+	
+	
+}
