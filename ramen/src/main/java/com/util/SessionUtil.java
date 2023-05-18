395a4602722dcd155a095b0e42c8ca7ca60d08ad
kun <sus5330@icloud.com>
2023-05-18 14:45:53 +0900

    중간

--- ramen/src/main/java/com/util/SessionUtil.java
+++ ramen/src/main/java/com/util/SessionUtil.java
@@ -1,2 +1,19 @@
-package com.util;public class SessionUtil {
+package com.util;
+
+import com.DTO.Member;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionUtil {
+	public static Long getMemberIdFromSession(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		Object sessionMember = session.getAttribute("member");
+
+		Long memberId = null;
+		if (sessionMember != null) {
+			memberId = ((Member) sessionMember).getMemberId();
+		}
+		return memberId;
+	}
 }
