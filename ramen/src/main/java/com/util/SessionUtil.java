8633180d3f1eb276077591a35c2ce7dd19f8f305
kun <sus5330@icloud.com>
2023-05-18 14:58:32 +0900

    SessionUtil 수정

--- ramen/src/main/java/com/util/SessionUtil.java
+++ ramen/src/main/java/com/util/SessionUtil.java
@@ -7,13 +7,47 @@ import javax.servlet.http.HttpSession;
 
 public class SessionUtil {
 	public static Member getMemberFromSession(HttpServletRequest req) {
-		HttpSession session = req.getSession();
-		Object sessionMember = session.getAttribute("member");
+		Object sessionMember = getSessionMember(req);
 
 		Member member = null;
 		if (sessionMember != null) {
 			member = ((Member) sessionMember);
 		}
 		return member;
+	}
+
+	public static Long getMemberRollFromSession(HttpServletRequest req) {
+		Object sessionMember = getSessionMember(req);
+
+		Long memberRoll = null;
+		if (sessionMember != null) {
+			memberRoll = ((Member) sessionMember).getRoll();
+		}
+		return memberRoll;
+	}
+
+	public static Long getMemberIdFromSession(HttpServletRequest req) {
+		Object sessionMember = getSessionMember(req);
+
+		Long memberId = null;
+		if (sessionMember != null) {
+			memberId = ((Member) sessionMember).getMemberId();
+		}
+		return memberId;
+	}
+
+	public static String getMemberNickNameFromSession(HttpServletRequest req) {
+		Object sessionMember = getSessionMember(req);
+
+		String memberNickName = null;
+		if (sessionMember != null) {
+			memberNickName = ((Member) sessionMember).getNickName();
+		}
+		return memberNickName;
+	}
+
+	private static Object getSessionMember(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		return session.getAttribute("member");
 	}
 }
