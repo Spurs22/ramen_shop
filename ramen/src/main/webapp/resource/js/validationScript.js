e304bc0a10d80895bdc50a161ea406e18eb0146c
kun <sus5330@icloud.com>
2023-05-25 20:04:44 +0900

    h

--- ramen/src/main/webapp/resource/js/validationScript.js
+++ ramen/src/main/webapp/resource/js/validationScript.js
@@ -1,14 +1,14 @@
+// 1보다 큰 숫자로 구성되어있는지 검사
 function isDigitNumber(num) {
     return /^\d+$/.test(num);
 }
 
+// 가격 포맷에 맞는지 검사
 function isPriceFormat(num) {
     return /^[1-9]\d*0$/.test(num);
 }
 
+// 빈 값인지 검사
 function isNull(input) {
-    if (input === null || input === "") {
-        return true;
-    }
-    return false;
+    return input === null || input === "";
 }
